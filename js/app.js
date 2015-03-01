eaa486736b5cf40a8ec130064dbc03f4bb8bb7e1
facu.tk <facu.tk@gmail.com>
2015-03-01 16:07:48 -0300

    Cambio el estilo para que matchee a Facebook

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-angular.module('tinderGiftApp', ['ngFacebook', 'firebase'])
+angular.module('tinderGiftApp', ['ngFacebook', 'firebase','ngRoute'])
+.value('fbURL', 'https://tindergift.firebaseio.com/')
 .config(['$facebookProvider', function($facebookProvider) {
     $facebookProvider.setAppId('342947875890308').setPermissions(['email','user_friends']);
 }])
@@ -7,13 +8,82 @@ angular.module('tinderGiftApp', ['ngFacebook', 'firebase'])
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
     $rootScope.$on('fb.load', function() {
         $window.dispatchEvent(new Event('fb.load'));
     });
 }])
+.service('fbRef', function(fbURL) {
+  return new Firebase(fbURL)
+})
+.service('Cards', function(fbURL, $firebase){
+    return $firebase(new Firebase(fbURL + 'card/')).$asArray();
+})
+.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            controller:'LandingCtrl',
+            templateUrl:'landing.html',
+        })
+
+        .when('/list', {
+            controller:'ListCtrl',
+            templateUrl:'list.html',
+        })
+
+        .when('/edit/:cardId', {
+              controller:'EditCtrl',
+              templateUrl:'detail.html'
+        })
+
+        .when('/new', {
+              controller:'CreateCtrl',
+              templateUrl:'detail.html'
+        })
+        .otherwise({
+            redirectTo:'/'
+        });
+})
+
+.controller('LandingCtrl', function($scope) {
+    
+})
+
+.controller('ListCtrl', function($scope, Cards) {
+    $scope.cards = Cards;
+})
+ 
+.controller('CreateCtrl', function($scope, $location, Cards) {
+    $scope.save = function() {
+        Cards.$add($scope.card).then(function(data) {
+            $location.path('/');
+        });
+    };
+})
+
+.controller('EditCtrl', function($scope, $location, $routeParams, Cards) {
+        var cardId = $routeParams.cardId, cardIndex;
+     
+        $scope.cards = Cards;
+        cardIndex = $scope.cards.$indexFor(cardId);
+        $scope.card = $scope.cards[cardIndex];
+     
+        $scope.destroy = function() {
+            $scope.cards.$remove($scope.card).then(function(data) {
+                $location.path('/');
+            });
+        };
+     
+        $scope.save = function() {
+            $scope.cards.$save($scope.card).then(function(data) {
+                $location.path('/');
+            });
+        };
+})
+
 .controller('myCtrl', ['$scope', '$facebook', '$firebase', function($scope, $facebook, $firebase) {
 
     $scope.$on('fb.auth.authResponseChange', function() {
@@ -30,7 +100,8 @@ angular.module('tinderGiftApp', ['ngFacebook', 'firebase'])
 
     $scope.getFriends = function() {
         if(!$scope.status) return;
-        $facebook.cachedApi('/me/friends').then(function(friends) {
+        $facebook.api('/me/friends').then(function(friends) {
+            console.log( friends );
             $scope.friends = friends.data;
         });
     };
