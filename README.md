cd9c30a7a63f25ee593d02f955f58411deb0daac
unknown <facundo.tkaczyszyn@ARBUEWRK039.politec.com.ar>
2015-03-02 17:34:01 -0300

    - Quito el color gris de fondo - Quito el boton Configuracion de la barrita - Ordeno un poco mejor el form para presentar un producto - Agrego un modulo python para parsear articulos de mercadolibre

--- README.md
+++ README.md
@@ -1,17 +1,54 @@
 # tinderGift
 Tinder for Gifts
-(https://apps.facebook.com/tindergift)[https://apps.facebook.com/tindergift]
+
+[https://apps.facebook.com/tindergift](https://apps.facebook.com/tindergift)
 
 # Instalacion
-* sudo apt-get install git
-* sudo apt-get install nodejs
-* sudo apt-get install npm
-* sudo npm install -g firebase-tools
-* git clone https://github.com/facutk/tinderGift.git 
-* cd tinderGift
+```sh
+sudo apt-get install git
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g firebase-tools
+git clone https://github.com/facutk/tinderGift.git 
+cd tinderGift
+python -m SimpleHTTPServer
+```
 
 # Mantenimiento
-* git add .
-* git commit -m "razon del cambio"
-* git push origin master
-* firebase deploy
+```sh
+git add .
+git commit -m "razon del cambio"
+git push origin master
+firebase deploy
+```
+
+# Mercadolibre
+```sh
+curl https://facutk.alwaysdata.net/mercadolibre/<URL_ARTICULO>
+```
+
+```json
+{ 
+    status: "ok | error",
+    name: "item name",
+    price: "$ XXX",
+    thumbnail: "url",
+    images: ['url1','urlN']
+}
+```
+### Example
+```sh
+curl https://facutk.alwaysdata.net/mercadolibre/articulo.mercadolibre.com.ar/MLA-547757806-attiny2313-20pu-dip20-atmel-mcu-avr-_JM
+```
+### Returns
+```json
+{
+  "images": [
+    "http:\/\/mla-s1-p.mlstatic.com\/attiny2313-20pu-dip20-atmel-mcu-avr-17456-MLA20138648926_082014-O.jpg"
+  ], 
+  "name": "Attiny2313-20pu Dip20 Atmel Mcu Avr", 
+  "price": "$ 42.00", 
+  "status": "ok", 
+  "thumbnail": "http:\/\/mla-s1-p.mlstatic.com\/attiny2313-20pu-dip20-atmel-mcu-avr-17456-MLA20138648926_082014-O.jpg"
+}
+```
---
+++ extras/mercadolibre.py
@@ -0,0 +1,61 @@
+from BeautifulSoup import BeautifulSoup
+import requests
+
+def get_ml_data( ml_url ):
+    if ml_url.find("mercadolibre"):
+        if ml_url[0:4] != "http":
+            ml_url = "http://" + ml_url
+
+        ret = {}
+        ret["status"] = "Error"
+
+        html = requests.get( ml_url ).text
+        if not html:
+            ret["description"] = "no html found"
+            return ret
+
+        soup = BeautifulSoup( html )
+        name = soup.find('h1', {'itemprop': 'name'})
+
+        if name:
+            name = name.contents[0]
+        else:
+            ret["description"] = "no name found"
+            return ret
+
+        pricetag = soup.find('article', {'class': 'price ch-price '})
+        if not pricetag:
+            ret["description"] = "no pricetag found"
+            return ret
+        else:
+            pricetag = pricetag.strong
+            price_number = pricetag.contents[0].strip()
+            price_decimal = pricetag.sup.contents[0]
+            price = price_number + '.' + price_decimal
+
+        images = []
+        imgs = soup.find('figure', {'id': 'gallery_dflt'}).findAll('img')
+        for img in imgs:
+            img_src = img['src']
+            if img_src[0:4] != "data":
+                images.append( img_src )
+        images = list( set( images ) )
+
+        if images:
+            thumbnail = images[0]
+
+        if not thumbnail:
+            ret["description"] = "no image found"
+            return ret
+
+        ret['status'] = "ok"
+        ret['thumbnail'] = thumbnail
+        ret['name'] = name
+        ret['price'] = price
+        ret['images'] = images
+        return ret
+
+if __name__ == '__main__':
+    url = "articulo.mercadolibre.com.ar/MLA-547757806-attiny2313-20pu-dip20-atmel-mcu-avr-_JM"
+    data = get_ml_data( url )
+    print data
